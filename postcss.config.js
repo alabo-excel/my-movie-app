240229983820d043ac75240743d4e7393e16b86b
alabo-excel <iamalaboexcel@gmail.com>
2021-02-04 23:20:52 +0100

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// const tailwindcss = require("tailwindcss");
+
+// module.exports = {
+//     plugins: [
+//         tailwindcss("./tailwind.config.js"),
+//     ]
+// };
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]
+}
---
+++ src/components/movie.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <div v-for="(movie, index) in movies" :key="index">
+      <p>{{movie.title}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'movie',
+  data: ()=> ({
+    movies: []
+
+  }),
+  mounted(){
+    fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=890a77e69b7432001d5a0fb13d5c720c&language=en-US&page=1')
+    .then(response => response.json())
+    .then(data => {
+      this.movies = data.results;
+    })
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
